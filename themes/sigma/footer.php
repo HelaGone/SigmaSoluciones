4848c25ce64355bccb82ee252185907bfac157ee
HelaGone <luah613@gmail.com>
2020-09-10 02:07:34 -0500

    footer markup

--- themes/sigma/footer.php
+++ themes/sigma/footer.php
@@ -1,4 +1,63 @@
-		<footer>
+		<footer class="inner_section footer_style">
+			<section id="big_footer" class="blogpost_item">
+				<div class="left-side">
+					<img width="36px" height="64" src="<?php echo THEMEPATH .'images/logo_sigma_w.svg' ?>" alt="">
+					<p>
+						SIGMA es un equipo multidisciplinario encargado
+						de brindarte soluciones integrales, usando
+						estrategias centradas en ti y tu familia
+					</p>
+				</div>
+				<div class="right-side">
+					<h4>Contacto</h4>
+					<ul>
+						<li>
+							<span>Email: </span>
+							<a href="#" title="">sigma.gpo@gmail.com</a>
+						</li>
+						<li>
+							<span>Teléfono: </span>
+							<a href="#" title="">55345-67890</a>
+						</li>
+						<li>
+							<span>Dirección: </span>
+							<a href="#" title="">Av. Cerro de las campanas, 3 <br/> San Andrés Atenco, Edo. Mex.</a>
+						</li>
+					</ul>
+				</div>
+				<div class="grid_object_header">
+					<ul>
+						<li>
+							<a href="#" title="Términos y condiciones">Términos y condiciones</a>
+						</li>
+						<li>
+							<a href="#" title="Políticas de privacidad">Políticas de privacidad</a>
+						</li>
+						<li>
+							<a href="#" title="Acerca de">Acerca de</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<section class="mini_footer">
+				<ul class="social_icons">
+					<li class="icon_item">
+						<a href="#" title="Facebook">
+							<img width="24" height="24" src="" alt="Facebook">
+						</a>
+					</li>
+					<li class="icon_item">
+						<a href="#" title="Twitter">
+							<img width="24" height="24" src="" alt="Twitter">
+						</a>
+					</li>
+					<li class="icon_item">
+						<a href="#" title="Instagram">
+							<img width="24" height="24" src="" alt="Instagram">
+						</a>
+					</li>
+				</ul>
+			</section>
 			<?php wp_footer(); ?>
 		</footer>
 	</body>
