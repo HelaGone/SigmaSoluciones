02ddf2f8ab6ac06551aa0e9584a5dddbe1ef24de
HelaGone <luah613@gmail.com>
2020-09-11 02:54:28 -0500

    big footer mobile

--- themes/sigma/footer.php
+++ themes/sigma/footer.php
@@ -1,16 +1,17 @@
 		<footer class="inner_section footer_style">
-			<section id="big_footer" class="blogpost_item">
-				<div class="left-side">
-					<img width="36px" height="64" src="<?php echo THEMEPATH .'images/logo_sigma_w.svg' ?>" alt="">
+
+			<section class="big_footer inner_wrapper">
+				<div class="sig_about_foot">
+					<img width="36" height="64" src="<?php echo THEMEPATH .'images/logo_sigma_w.svg' ?>" alt="">
 					<p>
 						SIGMA es un equipo multidisciplinario encargado
 						de brindarte soluciones integrales, usando
 						estrategias centradas en ti y tu familia
 					</p>
 				</div>
-				<div class="right-side">
+				<div class="sig_contact_foot">
 					<h4>Contacto</h4>
-					<ul>
+					<ul class="_comms">
 						<li>
 							<span>Email: </span>
 							<a href="#" title="">sigma.gpo@gmail.com</a>
@@ -21,39 +22,38 @@
 						</li>
 						<li>
 							<span>Dirección: </span>
-							<a href="#" title="">Av. Cerro de las campanas, 3 <br/> San Andrés Atenco, Edo. Mex.</a>
+							<a href="#" title="">Av. Cerro de las campanas, 3 San Andrés Atenco, Edo. Mex.</a>
 						</li>
 					</ul>
 				</div>
-				<div class="grid_object_header">
-					<ul>
-						<li>
-							<a href="#" title="Términos y condiciones">Términos y condiciones</a>
-						</li>
-						<li>
-							<a href="#" title="Políticas de privacidad">Políticas de privacidad</a>
-						</li>
-						<li>
-							<a href="#" title="Acerca de">Acerca de</a>
-						</li>
-					</ul>
-				</div>
+				<ul class="_disclaimers">
+					<li>
+						<a href="#" title="Términos y condiciones">Términos y condiciones</a>
+					</li>
+					<li>
+						<a href="#" title="Políticas de privacidad">Políticas de privacidad</a>
+					</li>
+					<li>
+						<a href="#" title="Acerca de">Acerca de</a>
+					</li>
+				</ul>
 			</section>
+
 			<section class="mini_footer">
 				<ul class="social_icons">
 					<li class="icon_item">
-						<a href="#" title="Facebook">
-							<img width="24" height="24" src="" alt="Facebook">
+						<a href="" title="Facebook">
+							<img width="24" height="24" src="<?php echo THEMEPATH .'images/social/white/fb.svg' ?>" alt="Facebook">
 						</a>
 					</li>
 					<li class="icon_item">
-						<a href="#" title="Twitter">
-							<img width="24" height="24" src="" alt="Twitter">
+						<a href="" title="Twitter">
+							<img width="24" height="24" src="<?php echo THEMEPATH .'images/social/white/tw.svg' ?>" alt="Twitter">
 						</a>
 					</li>
 					<li class="icon_item">
-						<a href="#" title="Instagram">
-							<img width="24" height="24" src="" alt="Instagram">
+						<a href="" title="Instagram">
+							<img width="24" height="24" src="<?php echo THEMEPATH .'images/social/white/ig.svg' ?>" alt="Instagram">
 						</a>
 					</li>
 				</ul>
