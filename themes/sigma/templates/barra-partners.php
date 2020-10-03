d3549a975d746647627d29e46bd54c13cbb17a18
HelaGone <luah613@gmail.com>
2020-10-02 20:13:24 -0500

    logos companies

--- themes/sigma/templates/barra-partners.php
+++ themes/sigma/templates/barra-partners.php
@@ -2,22 +2,16 @@
 <section id="partners" class="partners_wrapper">
   <div class="companies_carousel">
     <picture class="image_item_frame square">
-      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/centro.png'; ?>" alt="Logo CENTRO">
+      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/co_logo.png'; ?>" alt="Logo CENTRO">
     </picture>
     <picture class="image_item_frame square">
-      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/LOGO_NEW_BLACK.png'; ?>" alt="Logo NEW BLACK">
+      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/jmm_logo.png'; ?>" alt="Logo NEW BLACK">
     </picture>
     <picture class="image_item_frame square">
-      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/sigma_logo_hsqua.png'; ?>" alt="Logo SIGMA">
+      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/satelite_logo.png'; ?>" alt="Logo SIGMA">
     </picture>
     <picture class="image_item_frame square">
-      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/centro.png'; ?>" alt="Logo CENTRO">
-    </picture>
-    <picture class="image_item_frame square">
-      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/LOGO_NEW_BLACK.png'; ?>" alt="Logo NEW BLACK">
-    </picture>
-    <picture class="image_item_frame square">
-      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/sigma_logo_hsqua.png'; ?>" alt="Logo SIGMA">
+      <img width="90" height="90" src="<?php echo THEMEPATH . 'images/companies/ving_logo.png'; ?>" alt="Logo CENTRO">
     </picture>
   </div>
 </section>
